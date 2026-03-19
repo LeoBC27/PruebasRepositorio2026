6e2b1acfdd0baf9edacd74ed8bf0aa2390f4b462
Leonardo Barron Canel <l23450183@itz.edu.mx>
2026-03-18 21:19:35 -0600

    Cambio de RegEx
    Se cambio la RegEx para encontrar Lexemas y también se agregaron los Lexemas a encontrar

--- analizadorLexico/analizadorlexico/control/ProcesadorTexto.java
+++ analizadorLexico/analizadorlexico/control/ProcesadorTexto.java
@@ -14,8 +14,9 @@ public class ProcesadorTexto {
         StringBuilder resultados = new StringBuilder();
         int cantIDs = 0;
         int cantNum = 0;
+        int cantOtros= 0;
 
-        String regex = "([A-Za-z]\\w*)|(0|[1-9]\\d*)";
+        String regex = "([A-Za-z]\\w*)|(0|[1-9]\\d*)|(==|!=|<=|>=|<|>|=|\\+|-|\\.|,|;|\\(|\\))";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
 
@@ -31,9 +32,35 @@ public class ProcesadorTexto {
                 cantNum++;
                 resultados.append("Número: ").append(hallazgo).append("\n");
             }
+            else if (matcher.group(3) != null){
+                cantOtros++;
+                String nombreSimb = nombreLexema(hallazgo);
+                resultados.append(nombreSimb).append(": ").append(hallazgo).append("\n");
+            }
         }
 
-        return "IDs: " + cantIDs + " | Números: " + cantNum +
+        return "IDs: " + cantIDs + " | Números: " + cantNum + " | Otros Símbolos: " + cantOtros +
                "\n\nElementos encontrados en orden:\n" + resultados.toString();
+    }
+    private String nombreLexema(String simbolo){
+        switch (simbolo){
+            case "==": return "IGUAL_IGUAL";
+            case "!=": return "DIFERENTE_DE";
+            case "<=": return "MENOR_IGUAL_QUE";
+            case ">=": return "MAYOR_IGUAL_QUE";
+            case "<":  return "MENOR_QUE";
+            case ">":  return "MAYOR_QUE";
+            case "=":  return "IGUAL";
+            case "+":  return "SUMA";
+            case "-":  return "RESTA";
+            case "*":  return "MULT";
+            case "/":  return "DIVISION";
+            case ".":  return "PUNTO";
+            case ",":  return "COMA";
+            case ";":  return "PUNTO_COMA";
+            case "(":  return "PARENTESIS_ABRE";
+            case ")":  return "PARENTESIS_CIERRA";
+            default:   return "DESCONOCIDO";
+        }
     }
 }
