9d313b2bb73d14d333410c09c3c9f5c2545b6c79
Leonardo Barron Canel <l23450183@itz.edu.mx>
2026-03-18 21:24:50 -0600

    Clase Lexemas
    Amigo no se que paso se me borro la clase Lexemas y los valores que pusimos pero ya la hice de nuevo

---
+++ lexemas/Lexemas.java
@@ -0,0 +1,24 @@
+public class Lexemas {
+    
+    public static final int ID = 14;
+    public static final int NUM = 7;
+
+    public static final int IGUAL_IGUAL = 12;
+    public static final int DIFERENTE_DE = 29;
+    public static final int MENOR_IGUAL_QUE = 17;
+    public static final int MENOR_QUE = 4;
+    public static final int MAYOR_QUE = 50;
+    public static final int MAYOR_IGUAL_QUE = 8;
+
+    public static final int SUMA = 36;
+    public static final int RESTA = 25;
+    public static final int MULT = 41;
+    public static final int DIVISION = 13;
+    
+    public static final int PUNTO = 20;
+    public static final int COMA = 47;
+    public static final int IGUAL = 33;
+    public static final int PUNTO_COMA = 1;
+    public static final int PARENTESIS_ABRE = 10;
+    public static final int PARENTESIS_CIERRA = 26;
+} 
