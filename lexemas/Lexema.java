6f570698c999dd6707d02adb082b04e8064e7c05
Daniel Mireles Hernandez <danielmireleshdz34@gmail.com>
2026-03-25 08:45:42 -0600

    ¡¡¡¡NO BORRAR!!!
    SON LAS COSAS QUE ESTAMOS VIENDO EN CLASE SOBRE EL PROYECTO, POR FAVOR NO BORRAR

--- lexemas/Lexema.java
+++ lexemas/Lexema.java
@@ -1,6 +1,35 @@
 package PruebasRepositorio2026.lexemas;
 
 public class Lexema {
+    private String dato;
+    private String tipo;
+
+    // Constructor
+    public Lexema(String dato, String tipo) {
+        this.dato = dato;
+        this.tipo = tipo;
+    }
+    //getters and setters
+    public String getDato() {
+        return dato;
+    }
+
+    public void setDato(String dato) {
+        this.dato = dato;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
     
+    @Override
+    public String toString() {
+        return "[" + dato + "\t" + tipo + "]";
+    }
+        
     
 }
---
+++ lexemas/PruebasClase.java
@@ -0,0 +1,23 @@
+package PruebasRepositorio2026.lexemas;
+
+public class PruebasClase {
+        public static void main(String[] args) {
+        /* 
+        System.out.println(Lexemas.palabrasReservadas[0]);
+        System.out.println(Lexemas.palabrasReservadas[1]);
+        System.out.println(Lexemas.palabrasReservadas[2]);
+        System.out.println(Lexemas.palabrasReservadas[3]);
+        if (Lexemas.palabrasReservadas[0].equals("CONST")) {
+            System.out.println("Son iguales");
+        }//equals
+        Lexemas.llenaPalRes();
+        if (Lexemas.palabraRes.contains("do")){
+            System.out.println("Si lo contiene"); 
+        } */
+        Lexema l1 = new Lexema("Computadora", "Portatil");
+        System.out.println(l1);
+    }
+    
+}
+
+    
